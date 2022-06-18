831491701549c3a4be00c18b8e3bd934b7b7ffef
Lee <leeuklee@gmail.com>
2022-06-18 20:32:40 +0200

    added diagram

--- main.py
+++ main.py
@@ -6,26 +6,26 @@ rtc = RTC()
 rtc.datetime((2017, 8, 23, 1, 12, 48, 0, 0))  # set a specific date and time
 
 
-def run_pattern(sequence):
-    if sequence == "0":
+def run_pattern(pattern):
+    if pattern == "0":
         patterns.windmill(0.1)
-    if sequence == "1":
+    if pattern == "1":
         patterns.night_rider(0.05)
-    if sequence == "2":
+    if pattern == "2":
         patterns.spiral(0)
-    if sequence == "3":
+    if pattern == "3":
         patterns.chase_edge(0.05)
-    if sequence == "4":
+    if pattern == "4":
         patterns.l_r_fill(0.1)
-    if sequence == "5":
+    if pattern == "5":
         patterns.r_l_fill(0.1)
-    if sequence == "6":
+    if pattern == "6":
         patterns.l_r_sweep(0.1)
-    if sequence == "7":
+    if pattern == "7":
         patterns.r_l_sweep(0.1)
-    if sequence == "8":
+    if pattern == "8":
         patterns.t_b_fill(0.1)
-    if sequence == "9":
+    if pattern == "9":
         patterns.b_t_fill(0.1)
 
 
@@ -35,5 +35,4 @@ def randomish_number():
 
 
 while True:
-    sequence = randomish_number()
-    run_pattern(sequence)
+    run_pattern(randomish_number())
