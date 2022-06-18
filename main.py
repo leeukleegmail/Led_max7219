9571628e6285e7a079985ad6e812f6c132031d37
Lee <leeuklee@gmail.com>
2022-06-18 20:30:26 +0200

    added diagram

--- main.py
+++ main.py
@@ -1,32 +1,39 @@
 from machine import RTC
 
-from patterns import windmill, night_rider, r_l_fill, l_r_fill, chase_edge, spiral, r_l_sweep, l_r_sweep, t_b_fill, \
-    b_t_fill
+import patterns
 
 rtc = RTC()
 rtc.datetime((2017, 8, 23, 1, 12, 48, 0, 0))  # set a specific date and time
-datetime = rtc.datetime()[-1]  # get date and time
+
+
+def run_pattern(sequence):
+    if sequence == "0":
+        patterns.windmill(0.1)
+    if sequence == "1":
+        patterns.night_rider(0.05)
+    if sequence == "2":
+        patterns.spiral(0)
+    if sequence == "3":
+        patterns.chase_edge(0.05)
+    if sequence == "4":
+        patterns.l_r_fill(0.1)
+    if sequence == "5":
+        patterns.r_l_fill(0.1)
+    if sequence == "6":
+        patterns.l_r_sweep(0.1)
+    if sequence == "7":
+        patterns.r_l_sweep(0.1)
+    if sequence == "8":
+        patterns.t_b_fill(0.1)
+    if sequence == "9":
+        patterns.b_t_fill(0.1)
+
+
+def randomish_number():
+    datetime = rtc.datetime()[-1]
+    return str(datetime)[-1]
+
 
 while True:
-    datetime = rtc.datetime()[-1]
-    sequence = str(datetime)[-1]
-    if sequence == "0":
-        windmill(0.1)
-    if sequence == "1":
-        night_rider(0.05)
-    if sequence == "2":
-        spiral(0)
-    if sequence == "3":
-        chase_edge(0.05)
-    if sequence == "4":
-        l_r_fill(0.1)
-    if sequence == "5":
-        r_l_fill(0.1)
-    if sequence == "6":
-        l_r_sweep(0.1)
-    if sequence == "7":
-        r_l_sweep(0.1)
-    if sequence == "8":
-        t_b_fill(0.1)
-    if sequence == "9":
-        b_t_fill(0.1)
+    sequence = randomish_number()
+    run_pattern(sequence)
